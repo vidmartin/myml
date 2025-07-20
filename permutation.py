45fb6628721422267363c0795feed3efd95529ef
vidmartin <martin.vidmar@seznam.cz>
2025-07-20 13:05:10 +0200

    master: Permutation

--- permutation.py
+++ permutation.py
@@ -1,8 +1,9 @@
 
+from __future__ import annotations
 from typing import *
 import dataclasses
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True, eq=True)
 class Permutation:
     permutation: tuple[int, ...]
 
@@ -10,12 +11,30 @@ class Permutation:
         assert all(x >= 0 for x in self.permutation)
         assert all(x < len(self.permutation) for x in self.permutation)
     
+    def inverse(self) -> Permutation:
+        inverse_permutation = [0] * len(self.permutation)
+        for i in range(len(self.permutation)):
+            inverse_permutation[self.permutation[i]] = i
+        return Permutation(inverse_permutation)
+    
+    def compose(self, other: Permutation) -> Permutation:
+        assert len(self.permutation) == len(other.permutation)
+        return Permutation(tuple(
+            self.permutation[other.permutation[i]]
+            for i in range(len(self.permutation))
+        ))
+
     @staticmethod
-    def bring_to_front(indices: tuple[int, ...]) -> "Permutation":
-        """Return the permutation that brings to front the selected indices."""
-        raise NotImplementedError() # TODO
+    def identity(n_indices: int) -> Permutation:
+        """Return the identity permutation."""
+        return Permutation(tuple(range(n_indices)))
     
     @staticmethod
-    def bring_to_back(indices: tuple[int, ...]) -> "Permutation":
+    def bring_to_front(selected: tuple[int, ...], n_indices: int) -> Permutation:
+        """Return the permutation that brings to front the selected indices."""
+        return Permutation(selected + tuple(i for i in range(n_indices) if i not in selected))
+    
+    @staticmethod
+    def bring_to_back(selected: tuple[int, ...], n_indices: int) -> Permutation:
         """Return the permutation that brings to back the selected indices."""
-        raise NotImplementedError() # TODO
+        return Permutation(tuple(i for i in range(n_indices) if i not in selected) + selected)
---
+++ test_permutation.py
@@ -0,0 +1,47 @@
+
+from typing import *
+import unittest
+import numpy as np
+import permutation
+
+class PermutationTestCase(unittest.TestCase):
+    def setUp(self):
+        self._rng = np.random.default_rng(10012002)
+    
+    def test_inverse(self):
+        for i in range(10):
+            array = np.arange(i)
+            self._rng.shuffle(array)
+            perm = permutation.Permutation(tuple(array))
+            self.assertEqual(
+                perm.inverse().compose(perm),
+                permutation.Permutation.identity(i)
+            )
+            self.assertEqual(
+                perm.compose(perm.inverse()),
+                permutation.Permutation.identity(i)
+            )
+
+    def test_bringing(self):
+        arr = np.arange(5 ** 5).reshape((5,) * 5)
+
+        self.assertEqual(
+            permutation.Permutation.bring_to_front((), 5),
+            permutation.Permutation.identity(5),
+        )
+        self.assertEqual(
+            arr.transpose(permutation.Permutation.bring_to_front((3,), 5).permutation)[0,1,2,3,4],
+            arr[1,2,3,0,4]
+        )
+        self.assertEqual(
+            arr.transpose(permutation.Permutation.bring_to_back((3,), 5).permutation)[0,1,2,3,4],
+            arr[0,1,2,4,3]
+        )
+        self.assertEqual(
+            arr.transpose(permutation.Permutation.bring_to_front((1,2), 5).permutation)[0,1,2,3,4],
+            arr[2,0,1,3,4]
+        )
+        self.assertEqual(
+            arr.transpose(permutation.Permutation.bring_to_back((1,2), 5).permutation)[0,1,2,3,4],
+            arr[0,3,4,1,2]
+        )
