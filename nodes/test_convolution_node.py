cceaaee3422813bc230109ef474c31fa4c2bddd7
vidmartin <martin.vidmar@seznam.cz>
2025-08-06 15:46:31 +0200

    master: add tests for convolution derivative against input tensor

--- nodes/test_convolution_node.py
+++ nodes/test_convolution_node.py
@@ -144,7 +144,83 @@ class ConvolutionNodeTestCase(unittest.TestCase):
         self.assertTrue(np.allclose(res_3d_torch.detach().numpy(), res_3d_node.get_value()))
 
     def test_convolution_input_derivative(self):
-        pass # TODO
+        arr_1d = self._rng.standard_normal((100,10,50))
+        ker_size_1d = (13,)
+        ker_1d = self._rng.standard_normal(ker_size_1d)
+        pad_1d = (4,)
+        stride_1d = (3,)
+
+        arr_1d_torch = torch.tensor(arr_1d)
+        ker_1d_torch = torch.tensor(ker_1d)
+        res_1d_torch: torch.Tensor = torch.nn.functional.conv1d(
+            arr_1d_torch,
+            torch.stack((ker_1d_torch[torch.newaxis,...],) * 10),
+            stride=stride_1d,
+            padding=pad_1d,
+            groups=10,
+        )
+
+        arr_1d_node = nodes.ConstantNode(arr_1d)
+        ker_1d_node = nodes.ConstantNode(ker_1d)
+        res_1d_node = nodes.ConvolutionNode(arr_1d_node, ker_1d_node, pad_1d, stride_1d)
+
+        output_grad_1d = self._rng.standard_normal(res_1d_node.get_shape())
+        input_grad_1d, = res_1d_node.get_gradients_against([arr_1d_node], output_grad_1d)
+        res_1d_torch.backward(torch.tensor(output_grad_1d, requires_grad=False))
+
+        self.assertTrue(np.allclose(res_1d_torch.grad.detach().numpy(), input_grad_1d))
+
+        arr_2d = self._rng.standard_normal((100,10,50,50))
+        ker_size_2d = (13,11)
+        ker_2d = self._rng.standard_normal(ker_size_2d)
+        pad_2d = (4,3)
+        stride_2d = (3,4)
+
+        arr_2d_torch = torch.tensor(arr_2d)
+        ker_2d_torch = torch.tensor(ker_2d)
+        res_2d_torch = torch.nn.functional.conv2d(
+            arr_2d_torch,
+            torch.stack((ker_2d_torch[torch.newaxis,...],) * 10),
+            stride=stride_2d,
+            padding=pad_2d,
+            groups=10,
+        )
+
+        arr_2d_node = nodes.ConstantNode(arr_2d)
+        ker_2d_node = nodes.ConstantNode(ker_2d)
+        res_2d_node = nodes.ConvolutionNode(arr_2d_node, ker_2d_node, pad_2d, stride_2d)
+
+        output_grad_2d = self._rng.standard_normal(res_2d_node.get_shape())
+        input_grad_2d, = res_2d_node.get_gradients_against([arr_2d_node], output_grad_2d)
+        res_2d_torch.backward(torch.tensor(output_grad_2d, requires_grad=False))
+
+        self.assertTrue(np.allclose(res_2d_torch.grad.detach().numpy(), input_grad_2d))
+
+        arr_3d = self._rng.standard_normal((100,10,50,50,50))
+        ker_size_3d = (5,4,3)
+        ker_3d = self._rng.standard_normal(ker_size_3d)
+        pad_3d = (2,1,1)
+        stride_3d = (3,4,5)
+
+        arr_3d_torch = torch.tensor(arr_3d)
+        ker_3d_torch = torch.tensor(ker_3d)
+        res_3d_torch = torch.nn.functional.conv3d(
+            arr_3d_torch,
+            torch.stack((ker_3d_torch[torch.newaxis,...],) * 10),
+            stride=stride_3d,
+            padding=pad_3d,
+            groups=10,
+        )
+
+        arr_3d_node = nodes.ConstantNode(arr_3d)
+        ker_3d_node = nodes.ConstantNode(ker_3d)
+        res_3d_node = nodes.ConvolutionNode(arr_3d_node, ker_3d_node, pad_3d, stride_3d)
+
+        output_grad_3d = self._rng.standard_normal(res_3d_node.get_shape())
+        input_grad_3d, = res_3d_node.get_gradients_against([arr_3d_node], output_grad_3d)
+        res_3d_torch.backward(torch.tensor(output_grad_3d, requires_grad=False))
+
+        self.assertTrue(np.allclose(res_3d_torch.grad.detach().numpy(), input_grad_3d))
 
     def test_convolution_kernel_derivative(self):
         pass # TODO
