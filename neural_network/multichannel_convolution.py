b9b2e68c9bd7206bf5fde021ac844aa22c0aeb7a
vidmartin <martin.vidmar@seznam.cz>
2025-08-07 12:34:53 +0200

    master: add optional bias to MultichannelConvolutionModule

--- neural_network/multichannel_convolution.py
+++ neural_network/multichannel_convolution.py
@@ -16,27 +16,34 @@ class MultichannelConvolutionModule(NeuralNetwork[nodes.TensorNode]):
         out_channels: int,
         kernel_size: tuple[int, ...],
         padding: tuple[int, ...],
-        stride: tuple[int, ...]
+        stride: tuple[int, ...],
+        bias: bool = False
     ):
         self._in_channels = in_channels
         self._out_channels = out_channels
         self._kernel_size = kernel_size
         self._padding = padding
         self._stride = stride
+        self._bias = bias
     @override
     def get_params(self):
-        return {
+        kernels_dict = {
             f"ker_in{c_in}_out{c_out}": ParameterSpecification(
                 self._kernel_size
             )
             for c_in in range(self._in_channels)
             for c_out in range(self._out_channels)
         }
+        if self._bias:
+            return kernels_dict | {
+                "bias": ParameterSpecification((self._out_channels,))
+            }
+        return kernels_dict
     @override
     def _construct(self, input: nodes.TensorNode, params: Dict[str, np.ndarray], mode: EvaluationMode) -> ComputationalGraph:
         input_shape = input.get_shape()
         channel_dim = len(input_shape) - len(self._kernel_size) - 1
-        kernel_nodes = {
+        param_nodes = {
             key: nodes.ConstantNode(val)
             for key, val in params.items()
         }
@@ -46,21 +53,37 @@ class MultichannelConvolutionModule(NeuralNetwork[nodes.TensorNode]):
         ]
         output_plane_nodes = [
             nodes.ElementwiseNode(
-                elementwise.ElementwiseAdd(self._in_channels),
-                [
+                elementwise.ElementwiseAdd(self._in_channels), [
                     nodes.ConvolutionNode(
                         input_node=input_plane_node,
-                        kernel_node=kernel_nodes[f"ker_in{j}_out{i}"],
+                        kernel_node=param_nodes[f"ker_in{i}_out{o}"],
                         padding=self._padding,
                         stride=self._stride,
                     )
-                    for j, input_plane_node in enumerate(input_plane_nodes)
+                    for i, input_plane_node in enumerate(input_plane_nodes)
                 ]
             )
-            for i in range(self._out_channels)
+            for o in range(self._out_channels)
         ]
+
+        if self._bias:
+            bias_node = nodes.ConstantNode(params["bias"])
+            output_plane_nodes = [
+                nodes.ElementwiseNode(
+                    elementwise.ElementwiseAdd(2), [
+                        output_plane_node,
+                        nodes.ExtendNode(
+                            nodes.SliceNode(bias_node, 0, o),
+                            output_plane_node.get_shape(),
+                        )
+                    ]
+                )
+                for o, output_plane_node in enumerate(output_plane_nodes)
+            ]
+            param_nodes = param_nodes | { "bias": bias_node }
+
         output_node = nodes.StackNode(output_plane_nodes, channel_dim)
         return ComputationalGraph(
             output_node=output_node,
-            param_nodes=kernel_nodes,
+            param_nodes=param_nodes,
         )
--- neural_network/test_multichannel_convolution.py
+++ neural_network/test_multichannel_convolution.py
@@ -55,3 +55,52 @@ class MultichannelConvolutionTestCase(unittest.TestCase):
                 kernel_node_grad,
                 torch_conv2d.get_parameter("weight").grad.detach().numpy()[o,i,:,:]
             ))
+    def test_multichannel_convolution_2d_with_bias(self):
+        in_channels, out_channels = 10, 15
+        kersize = (8, 9)
+        stride = (2, 3)
+        pad = (1, 1)
+        input_arr = self._rng.standard_normal((50, in_channels, 100, 100))
+        kernel_arrs = (self._rng.random((out_channels, in_channels, *kersize)) * 2 - 1) * (6.0 / sum(kersize)) ** 0.5
+        bias_arr = (self._rng.random((out_channels,)) * 2 - 1) * (6.0 / (out_channels + 1)) ** 0.5
+
+        torch_conv2d = torch.nn.Conv2d(in_channels, out_channels, kersize, stride, pad, bias=True)
+        torch_conv2d.get_parameter("weight").data = torch.tensor(kernel_arrs, requires_grad=False)
+        torch_conv2d.get_parameter("bias").data = torch.tensor(bias_arr, requires_grad=False)
+        input_torch = torch.tensor(input_arr)
+        output_torch: torch.Tensor = torch_conv2d(input_torch)
+        sum_torch = output_torch.sum()
+        sum_torch.backward()
+
+        my_conv2d = MultichannelConvolutionModule(in_channels, out_channels, kersize, pad, stride, bias=True)
+        input_node = nodes.ConstantNode(input_arr)
+        graph = my_conv2d.construct(
+            input=input_node,
+            params={
+                f"ker_in{i}_out{o}": kernel_arrs[o,i,:,:]
+                for o in range(out_channels)
+                for i in range(in_channels)
+            } | { "bias": bias_arr },
+            mode=EvaluationMode.TRAINING
+        )
+        sum_node = nodes.SumNode(graph.output_node, 4)
+        kernel_nodes_linear = [
+            graph.param_nodes[f"ker_in{i}_out{o}"]
+            for i in range(in_channels)
+            for o in range(out_channels)
+        ]
+        kernel_nodes_grads = sum_node.get_gradients_against(kernel_nodes_linear)
+
+        self.assertTrue(np.allclose(output_torch.detach().numpy(), graph.output_node.get_value()))
+
+        for kernel_node_grad, (i, o) in zip(
+            kernel_nodes_grads,
+            itertools.product(range(in_channels), range(out_channels))
+        ):
+            self.assertTrue(np.allclose(
+                kernel_node_grad,
+                torch_conv2d.get_parameter("weight").grad.detach().numpy()[o,i,:,:]
+            ))
+
+        bias_node_grad, = sum_node.get_gradients_against([graph.param_nodes["bias"]])
+        self.assertTrue(np.allclose(bias_node_grad, torch_conv2d.get_parameter("bias").grad.detach().numpy(), atol=0.001))
