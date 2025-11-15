92a82b7eb3bbfd0c519cdeefc97e0d76bf44b89a
vidmartin <martin.vidmar@seznam.cz>
2025-11-15 16:41:17 +0100

    master: use SelectNode in SelectLastStateModule

--- neural_network/select_last_state.py
+++ neural_network/select_last_state.py
@@ -9,15 +9,28 @@ import elementwise
 TResult = TypeVar("TResult")
 
 class SelectLastStateModule(NeuralNetwork[nodes.TensorNode]):
+    def __init__(self, sequence_length_metadata_key: str | None):
+        """
+        `sequence_length_metadata_key`: if a string key is given, the corresponding metadata value will be used
+        as information about how long the individual sequences in the batch are. It will expect
+        the metadata value to be an array of integers with length corresponding to batch size. If set to
+        `None`, the length of all the sequences is taken to be the size of the input tensor along the corresponding dimension.
+        """
+        self._sequence_length_metadata_key = sequence_length_metadata_key
     @override
     def get_params(self):
         return {}
     @override
     def _construct(self, input: nodes.TensorNode, params: Dict[str, np.ndarray], mode: EvaluationMode, metadata: dict[str, Any]) -> ComputationalGraph:
         input_shape = input.get_shape()
-        output_node = nodes.SliceNode(input, len(input_shape) - 2, input_shape[-2] - 1)
-        # TODO: a way to select different item for each datapoint
-        return ComputationalGraph(output_node, {})
+        assert len(input_shape) == 3, f"only 3-dimensional arrays are supported by SelectLastStateModule"
+
+        if self._sequence_length_metadata_key is not None:
+            output_node = nodes.SelectNode(input, 0, 1, params[self._sequence_length_metadata_key])
+            return ComputationalGraph(output_node, {})
+        else:
+            output_node = nodes.SliceNode(input, len(input_shape) - 2, input_shape[-2] - 1)
+            return ComputationalGraph(output_node, {})
     @override
     def accept(self, visitor: NeuralNetworkVisitor[TResult]) -> TResult:
         raise NotImplementedError() # TODO
